b2064529568adbf776176089cbad46e2a2dc6663
Julianna <jchianelli@gmail.com>
2017-08-05 21:06:32 -0400

    Update day1.js

--- day1.js
+++ day1.js
@@ -1,14 +1,13 @@
-function main(){
- $('#greetings').hide();
- $('#greetings').fadeIn(1000);
-}
-
-
 
 $.when(main()).done(function cat(){
  $('#cat').hide();
  $('#cat').fadeIn(1000);
 });
+
+function main(){
+ $('#greetings').hide();
+ $('#greetings').fadeIn(1000);
+}
 
 
 $(document).ready(main);
