1468b1c6cafa902352b6a2fb847f575522bdc96b
Julianna <jchianelli@gmail.com>
2017-08-05 22:19:05 -0400

    Update day1.js

--- day1.js
+++ day1.js
@@ -1,12 +1,11 @@
-function main(){
+
  $('#greetings').hide();
  $('#greetings').fadeIn(1000);
-}
 
-function cat(){
+ 
  $('#cat').hide();
  $('#cat').fadeIn(1000);
-}
+
 
 $(document).ready(main);
 $(document).ready(cat);
