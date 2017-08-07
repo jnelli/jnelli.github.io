58219bb17d327a47779e0d8a1f7fda8cffe735dd
Julianna <jchianelli@gmail.com>
2017-08-06 20:54:55 -0400

    Day 2
    JS, CSS and HTML file uploads

---
+++ day2.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+  $("#hognose").mouseenter(function() {
+    $(this).effect("shake");
+  });
+})
+
+$(document).ready(function (){
+  $("#mouse").draggable({appendTo: "body"});
+})
+
+$(document).ready(function (){
+  $("#toad").draggable({appendTo: "body"});
+})
+
+$(document).ready(function (){
+  $("#grasshopper").draggable({appendTo: "body"});
+})
