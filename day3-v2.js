d4b856432fed46ae78aa897c078f889bb6277698
Julianna <jchianelli@gmail.com>
2017-08-07 20:03:19 -0400

    Add files via upload

---
+++ day3-v2.js
@@ -0,0 +1,28 @@
+var compOptions = ["rock", "paper", "scissors"];
+function scramble(){
+ return compOptions[Math.floor(Math.random() * compOptions.length)];
+}
+
+function game(){
+  var userChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+  var compChoice = scramble();
+  if (userChoice === "gun" || userChoice === "pistol"){
+    return "You selected " + userChoice + ". The computer selected " + compChoice + ". You've won, I guess, but isn't that cheating?";
+  } else if (userChoice === compChoice) {
+    return "You selected " + userChoice + ". The computer selected " + compChoice + ". It's a tie.";
+  } else if (compChoice === "rock" && userChoice != "paper"){
+    return "You selected " + userChoice + ". The computer selected " + compChoice + ". You've lost :(";
+  } else if (compChoice === "paper" && userChoice != "scissors"){
+    return "You selected " + userChoice + ". The computer selected " + compChoice + ". The computer has won. Try again!";
+  } else if (compChoice === "scissors" && userChoice != "rock"){
+    return "You selected " + userChoice + ". The computer selected " + compChoice + ". That's a loss. Want to play again?";
+  } else {
+    return "You selected " + userChoice + ". The computer selected " + compChoice + ". Congratulations, you've won!!";
+  }
+
+
+}
+
+function result(){
+   document.getElementById("outcome").innerHTML = game();
+}
