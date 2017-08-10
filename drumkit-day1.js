9c0914dce62de2edb2be8c4930d4aea1635eb264
Julianna <jchianelli@gmail.com>
2017-08-10 17:34:38 -0400

    Update drumkit-day1.js

--- drumkit-day1.js
+++ drumkit-day1.js
@@ -1,22 +1,19 @@
-function playSound(e){
-  //ES6 to create variable for pressed key and then key class
-  const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keycode}"]`);
-   //ES6 to create variable for pressed key
-  if(!audio) return; //cancels function if no audio w/ pressed key
-  audio.currentTime = 0; //Rewinds audio to start each time key is pressed
-  audio.play();
-  key.classList.add('playing'); //Same thing as jQuery's addClass!
+  function playSound (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keycode}"]`);
+    if(!audio)
+      return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');  
 }
 
-
-function removeTransition(e){
-  if (e.propertyName !== "transition") return;
-  transform
-  this.classList.remove("playing");
+function endSound (e) {
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!key)
+    return;
+  key.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll(".key");
-keys.forEach(key => addEventListener("transitionend", removeTransition));
-// => is another ES6 function
-window.addEventListener('keydown', playSound);
+  window.addEventListener('keydown', playSound)
+  window.addEventListener('keyup', endSound)
